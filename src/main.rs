14594037155fa7cf4efb1d9e7cd17f27d079a1f1
Shingo Kato <katomail0510@gmail.com>
2019-05-03 12:21:59 +0900

    Implement operator-precedence parser.

--- src/main.rs
+++ src/main.rs
@@ -7,45 +7,43 @@ mod lexer;
 mod parser;
 
 use lexer::{Lexer, Token};
-use parser::{BinaryExpNode, PrimaryNode};
+use parser::{parser, Node};
 
-fn binary_expression_parser(lhs_token: Token, rhs_token: Token, op_token: Token) -> BinaryExpNode {
-    let lhs = PrimaryNode { token: lhs_token };
-    let rhs = PrimaryNode { token: rhs_token };
-    BinaryExpNode {
-        op: op_token,
-        lhs,
-        rhs,
-    }
-}
+fn binary_expression_emitter(node: Node) {
+    match node {
+        Node::BinaryExp(node) => {
+            // initialize
+            let context = Context::create();
+            let module = context.create_module("my_module");
+            let builder = context.create_builder();
 
-fn binary_expression_emitter(node: BinaryExpNode) {
-    // initialize
-    let context = Context::create();
-    let module = context.create_module("my_module");
-    let builder = context.create_builder();
+            // generate function
+            let function =
+                module.add_function("main", context.i64_type().fn_type(&[], false), None);
+            let basic_block = context.append_basic_block(&function, "entry");
+            builder.position_at_end(&basic_block);
 
-    // generate function
-    let function = module.add_function("main", context.i64_type().fn_type(&[], false), None);
-    let basic_block = context.append_basic_block(&function, "entry");
-    builder.position_at_end(&basic_block);
+            // define main function
+            let i64_type = context.i64_type();
+            let lhs = *node.lhs;
+            let rhs = *node.rhs;
+            let const_x = i64_type.const_int(lhs.get_number(), false);
+            let const_y = i64_type.const_int(rhs.get_number(), false);
+            let ret = match node.op {
+                Token::Op(op, _) => match op.as_ref() {
+                    "+" => builder.build_int_add(const_x, const_y, "main"),
+                    "*" => builder.build_int_mul(const_x, const_y, "main"),
+                    _ => panic!("Operator not implemented."),
+                },
+                _ => panic!(),
+            };
+            builder.build_return(Some(&ret));
 
-    // define main function
-    let i64_type = context.i64_type();
-    let const_x = i64_type.const_int(node.lhs.get_number(), false);
-    let const_y = i64_type.const_int(node.rhs.get_number(), false);
-    let ret = match node.op {
-        Token::Op(op, _) => match op.as_ref() {
-            "+" => builder.build_int_add(const_x, const_y, "main"),
-            "*" => builder.build_int_mul(const_x, const_y, "main"),
-            _ => panic!("Operator not implemented."),
-        },
+            // print_to_file
+            let _ = module.print_to_file(path::Path::new("compiled.ll"));
+        }
         _ => panic!(),
-    };
-    builder.build_return(Some(&ret));
-
-    // print_to_file
-    let _ = module.print_to_file(path::Path::new("compiled.ll"));
+    }
 }
 
 fn compiler(code: String) {
@@ -53,10 +51,7 @@ fn compiler(code: String) {
     let lexer = Lexer::new();
     let mut tokens = lexer.lex(code);
     println!("{:?}", tokens);
-    let lhs = tokens.pop().unwrap();
-    let op = tokens.pop().unwrap();
-    let rhs = tokens.pop().unwrap();
-    let node = binary_expression_parser(lhs, rhs, op);
+    let node = parser(&mut tokens);
     binary_expression_emitter(node);
 }
 
--- src/parser.rs
+++ src/parser.rs
@@ -1,22 +1,95 @@
-use crate::lexer::Token;
+use crate::lexer::{Associativity, Token, Tokens};
+
+pub fn parser(tokens: &mut Tokens) -> Node {
+    binary_expression_parser(tokens)
+}
+
+pub fn binary_expression_parser(tokens: &mut Tokens) -> Node {
+    let node = BinaryExpNode::new(tokens);
+    node
+}
 
 #[derive(Debug, PartialEq, Clone)]
-pub struct PrimaryNode {
-    pub token: Token,
+pub enum Node {
+    Primary(PrimaryNode),
+    BinaryExp(BinaryExpNode),
 }
-impl PrimaryNode {
+impl Node {
+    fn new(tokens: &mut Tokens) -> Node {
+        Node::Primary(PrimaryNode::new(tokens))
+    }
     pub fn get_number(&self) -> u64 {
-        let node = self.clone();
-        match node.token {
-            Token::Num(num) => num.parse::<u64>().expect(""),
+        match self.clone() {
+            Node::Primary(node) => match node.token {
+                Token::Num(num) => num.parse::<u64>().expect(""),
+                _ => panic!(),
+            },
             _ => panic!(),
         }
     }
 }
 
 #[derive(Debug, PartialEq, Clone)]
+pub struct PrimaryNode {
+    pub token: Token,
+}
+impl PrimaryNode {
+    fn new(tokens: &mut Tokens) -> PrimaryNode {
+        let token = tokens.pop().unwrap();
+        PrimaryNode { token }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
 pub struct BinaryExpNode {
     pub op: Token,
-    pub lhs: PrimaryNode,
-    pub rhs: PrimaryNode,
+    pub lhs: Box<Node>,
+    pub rhs: Box<Node>,
+}
+impl BinaryExpNode {
+    pub fn new(tokens: &mut Tokens) -> Node {
+        let lhs = Node::new(tokens);
+        BinaryExpNode::binary_expression(lhs, tokens, 0)
+    }
+    fn binary_expression(mut lhs: Node, tokens: &mut Tokens, min_precedence: u32) -> Node {
+        while let Some(token) = tokens.peek() {
+            match token {
+                Token::Op(op, property) => {
+                    let (root_precedence, root_associativity) =
+                        (property.clone().precedence, property.clone().associativity);
+                    if root_precedence < min_precedence {
+                        break;
+                    }
+                    tokens.pop(); // consume op
+                    let op = Token::Op(op, property);
+                    // TODO: impl error handling
+                    let mut rhs = Node::new(tokens);
+                    while let Some(Token::Op(_, property2)) = tokens.peek() {
+                        let (precedence, _associativity) =
+                            (property2.precedence, property2.associativity);
+                        match root_associativity {
+                            Associativity::Right => {
+                                if root_precedence > precedence {
+                                    break;
+                                }
+                            }
+                            Associativity::Left => {
+                                if root_precedence >= precedence {
+                                    break;
+                                }
+                            }
+                        }
+                        rhs = BinaryExpNode::binary_expression(rhs, tokens, precedence)
+                    }
+                    lhs = Node::BinaryExp(BinaryExpNode {
+                        op,
+                        lhs: Box::new(lhs),
+                        rhs: Box::new(rhs),
+                    });
+                }
+                _ => break,
+            }
+        }
+        lhs
+    }
 }
